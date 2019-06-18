9b5332c650bec6842ff78f853413abdadfda61c4
daniel.dsantos <daniel.dsantos@b2wdigital.com>
2019-06-18 16:25:02 -0300

    added sommatory implementation and test

--- maas/app.py
+++ maas/app.py
@@ -6,4 +6,8 @@ app = App("", "", "", 1)
 
 @app.route(["/"], type=RouteTypes.HTTP, methods=["POST"])
 async def operation(request: web.Request):
-    return web.json_response({})
+    data = await request.json()
+    result = data['left'] + data['right']
+    return web.json_response({
+        'result': result
+    })
